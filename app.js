5d6386995e09142de305844df3ba8d4a6b44e959
Facundo Gordillo <facundogordillo@FMGordillo-MacBookPro.local>
2018-08-18 17:21:52 -0300

    Reconocimiento de audio implementado + refactoring

--- app.js
+++ app.js
@@ -14,31 +14,47 @@
  * limitations under the License.
  */
 
-'use strict';
+"use strict";
 
-var express = require('express'); // app server
-var bodyParser = require('body-parser'); // parser for post requests
-var AssistantV1 = require('watson-developer-cloud/assistant/v1'); // watson sdk
+const express = require("express"); // app server
+const bodyParser = require("body-parser"); // parser for post requests
+const AuthorizationV1 = require("watson-developer-cloud/authorization/v1"); // for Web side
+const AssistantV1 = require("watson-developer-cloud/assistant/v1"); // assistant sdk
+const SpeechToTextV1 = require("watson-developer-cloud/speech-to-text/v1"); // speech sdk
 
 var app = express();
 
 // Bootstrap application settings
-app.use(express.static('./public')); // load UI from public folder
+app.use(express.static("./public")); // load UI from public folder
 app.use(bodyParser.json());
 
 // Create the service wrapper
 
-var assistant = new AssistantV1({
-  version: '2018-07-10'
+const assistant = new AssistantV1({
+  version: "2018-07-10"
 });
 
+var sttAuthService = new AuthorizationV1(
+  Object.assign(
+    {
+      username: process.env.SPEECH_TO_TEXT_USERNAME,
+      password: process.env.SPEECH_TO_TEXT_PASSWORD
+    },
+    {}
+  )
+);
+
 // Endpoint to be call from the client side
-app.post('/api/message', function (req, res) {
-  var workspace = process.env.WORKSPACE_ID || '<workspace-id>';
-  if (!workspace || workspace === '<workspace-id>') {
+app.post("/api/message", function(req, res) {
+  var workspace = process.env.WORKSPACE_ID || "<workspace-id>";
+  if (!workspace || workspace === "<workspace-id>") {
     return res.json({
-      'output': {
-        'text': 'The app has not been configured with a <b>WORKSPACE_ID</b> environment variable. Please refer to the ' + '<a href="https://github.com/watson-developer-cloud/assistant-simple">README</a> documentation on how to set this variable. <br>' + 'Once a workspace has been defined the intents may be imported from ' + '<a href="https://github.com/watson-developer-cloud/assistant-simple/blob/master/training/car_workspace.json">here</a> in order to get a working application.'
+      output: {
+        text:
+          "The app has not been configured with a <b>WORKSPACE_ID</b> environment variable. Please refer to the " +
+          '<a href="https://github.com/watson-developer-cloud/assistant-simple">README</a> documentation on how to set this variable. <br>' +
+          "Once a workspace has been defined the intents may be imported from " +
+          '<a href="https://github.com/watson-developer-cloud/assistant-simple/blob/master/training/car_workspace.json">here</a> in order to get a working application.'
       }
     });
   }
@@ -49,7 +65,7 @@ app.post('/api/message', function (req, res) {
   };
 
   // Send the input to the assistant service
-  assistant.message(payload, function (err, data) {
+  assistant.message(payload, function(err, data) {
     if (err) {
       return res.status(err.code || 500).json(err);
     }
@@ -68,6 +84,24 @@ app.post('/api/message', function (req, res) {
 
     return res.json(updateMessage(payload, data));
   });
+});
+
+// Endpoint to be call from the client side
+app.get("/api/get_audio_token", function(req, res) {
+  sttAuthService.getToken(
+    {
+      url: SpeechToTextV1.URL
+    },
+    (err, token) => {
+      if (err) {
+        console.error("[SPEECH TOKEN]", error);
+        res.status(500).send("Error retrieving token");
+        return;
+      } else {
+        res.send(token);
+      }
+    }
+  );
 });
 
 /**
@@ -91,11 +125,11 @@ function updateMessage(input, response) {
     // user's intent . In these cases it is usually best to return a disambiguation message
     // ('I did not understand your intent, please rephrase your question', etc..)
     if (intent.confidence >= 0.75) {
-      responseText = 'I understood your intent was ' + intent.intent;
+      responseText = "I understood your intent was " + intent.intent;
     } else if (intent.confidence >= 0.5) {
-      responseText = 'I think your intent was ' + intent.intent;
+      responseText = "I think your intent was " + intent.intent;
     } else {
-      responseText = 'I did not understand your intent';
+      responseText = "I did not understand your intent";
     }
   }
   response.output.text = responseText;
